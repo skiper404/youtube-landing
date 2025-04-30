d7a052e7d8870d2c2640fce7d68941ab8dbfc2c9
Kirill Stetsenko <stetsenkoki@gmail.com>
2025-04-30 16:37:02 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div
+    class="h-screen bg-gradient-to-b from-neutral-100 to-neutral-900 text-center text-3xl text-neutral-600"
+  >
+    YouTube
+  </div>
+</template>
+
+<style scoped></style>
